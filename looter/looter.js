fc4590a48d9b4c86ee257363454b9d1e5362e24e
Martin.Knopf <Martin.Knopf@payone.de>
2014-08-25 16:09:22 +0200

    readme

--- looter/looter.js
+++ looter/looter.js
@@ -2,7 +2,8 @@ var Rx = require('rx')
   , moment = require('moment')
   , inquirer = require('inquirer')
   , _ = require('lodash')
-  , ui = new inquirer.ui.BottomBar();
+  , ui = new inquirer.ui.BottomBar()
+  , TICK = 2000;
 
 function sinPrice(x, min, max) { return Math.floor((Math.sin(x) + 1) / 2 * (max - min) + min); }
 
@@ -15,14 +16,14 @@ var randomPriceSource = Rx.Observable.generateWithRelativeTime(
   _.constant(true),
   _.constant(0),
   _.partial(_.random, 900, 1500, false),
-  _.constant(2000));
+  _.constant(TICK));
 
 var sinPriceSource = Rx.Observable.generateWithRelativeTime(
   sinPrice(_.random(1, 3), 1501, 900),
   _.constant(true),
   _.partial(add, 0.1),
   _.partialRight(sinPrice, 900, 1501),
-  _.constant(2000));
+  _.constant(TICK));
 
 var timestampSource = Rx.Observable.interval(100).map(time);
 
