328bda16d9879b916264bcc24c13f2807a1a674d
Martin.Knopf <Martin.Knopf@payone.de>
2014-08-25 17:00:53 +0200

    went to node-webkit

--- looter/looter.js
+++ looter/looter.js
@@ -1,8 +1,6 @@
 var Rx = require('rx')
   , moment = require('moment')
-  , inquirer = require('inquirer')
   , _ = require('lodash')
-  , ui = new inquirer.ui.BottomBar()
   , TICK = 2000;
 
 function sinPrice(x, min, max) { return Math.floor((Math.sin(x) + 1) / 2 * (max - min) + min); }
@@ -11,30 +9,18 @@ function add(x, d) { return x + d; }
 
 function time() { return moment().format('DD.MM.YYYY hh:mm:ss'); }
 
-var randomPriceSource = Rx.Observable.generateWithRelativeTime(
+exports.timestampSource = Rx.Observable.interval(100).map(time);
+
+exports.randomPriceSource = Rx.Observable.generateWithRelativeTime(
   _.random(900, 1500),
   _.constant(true),
   _.constant(0),
   _.partial(_.random, 900, 1500, false),
   _.constant(TICK));
 
-var sinPriceSource = Rx.Observable.generateWithRelativeTime(
+exports.sinPriceSource = Rx.Observable.generateWithRelativeTime(
   sinPrice(_.random(1, 3), 1501, 900),
   _.constant(true),
   _.partial(add, 0.1),
   _.partialRight(sinPrice, 900, 1501),
   _.constant(TICK));
-
-var timestampSource = Rx.Observable.interval(100).map(time);
-
-randomPriceSource
-
-sinPriceSource
-.map(function(value) {
-  return ''+value;
-})
-.subscribe(function(value) {
-  ui.updateBottomBar(value);
-})
-
-timestampSource
