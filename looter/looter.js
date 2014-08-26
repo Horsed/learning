5354e85ebf1509db14155e484b11592d615905d3
Martin.Knopf <Martin.Knopf@payone.de>
2014-08-26 17:49:41 +0200

    added browserify and grunt

--- looter/looter.js
+++ looter/looter.js
@@ -9,18 +9,22 @@ function add(x, d) { return x + d; }
 
 function time() { return moment().format('DD.MM.YYYY hh:mm:ss'); }
 
-exports.timestampSource = Rx.Observable.interval(100).map(time);
+var timestampSource = exports.timestampSource = Rx.Observable.interval(100).map(time);
 
-exports.randomPriceSource = Rx.Observable.generateWithRelativeTime(
+var randomPriceSource = exports.randomPriceSource = Rx.Observable.generateWithRelativeTime(
   _.random(900, 1500),
   _.constant(true),
   _.constant(0),
   _.partial(_.random, 900, 1500, false),
   _.constant(TICK));
 
-exports.sinPriceSource = Rx.Observable.generateWithRelativeTime(
+var sinPriceSource = exports.sinPriceSource = Rx.Observable.generateWithRelativeTime(
   sinPrice(_.random(1, 3), 1501, 900),
   _.constant(true),
   _.partial(add, 0.1),
   _.partialRight(sinPrice, 900, 1501),
   _.constant(TICK));
+
+var balance = exports.balance = sinPriceSource.map(function(price) {
+  return price * 15;
+})
