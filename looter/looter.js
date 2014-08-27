66fff4c5a2e72d918450ebc231b0d844a951f9da
Martin.Knopf <Martin.Knopf@payone.de>
2014-08-27 17:36:16 +0200

    bla

--- looter/looter.js
+++ looter/looter.js
@@ -3,57 +3,39 @@ var Rx = require('rx')
   , _ = require('lodash')
   , TICK = 2000;
 
-function sinPrice(min, max, x) { ;return Math.floor((Math.sin(x) + 1) / 2 * (max - min) + min); }
+function sinPrice(min, max, x) { return Math.floor((Math.sin(x) + 1) / 2 * (max - min) + min); }
 
-function add(x, d) { return x + d; }
+function randomize(price) { return Math.floor(_.random(price - _.random(900, 3500) / 2, price + _.random(900, 3500) / 2)); }
 
 function time() { return moment().format('DD.MM.YYYY hh:mm:ss'); }
 
 var gameLoop
   = exports.gameLoop
-  = Rx.Observable.interval(1000)
+  = Rx.Observable.interval(10)
 
 var timestampSource
   = exports.timestampSource
   = gameLoop
     .map(time)
 
-var randomPriceSource
-  = exports.randomPriceSource
-  = gameLoop
-    .map(_.partial(_.random, 900, 3500, false))
-
-var sinPriceSource
-  = exports.sinPriceSource
+var sinusTrend
   = gameLoop
     .map(function(time) { return time / 10; })
     .map(_.partial(sinPrice, 900, 3501))
 
-var balance
-  = exports.balance
-  = sinPriceSource
-  .combineLatest(
-    randomPriceSource,
-    sinPriceSource,
-    function(r, s) { return r + s; })
-
-var randomGraph
-  = exports.randomGraph
-  = Rx.Observable.combineLatest(
+var dot
+  = Rx.Observable.zip(
     gameLoop,
-    randomPriceSource,
-    function(time, price) { return {time: time*10, price: price / 100}; })
+    sinusTrend.map(randomize),
+    function(x, y) { return {x: x, y: y}; })
 
-var sinusGraph
-  = exports.sinusGraph
-  = Rx.Observable.combineLatest(
-    gameLoop,
-    sinPriceSource,
-    function(time, price) { return {time: time, price: price / 100}; })
-
-var balanceGraph
-  = exports.balanceGraph
-  = Rx.Observable.combineLatest(
-    gameLoop,
-    balance,
-    function(time, balance) { return {time: time, balance: balance / 100}; })
+var randSinus
+  = exports.randSinus
+  = dot
+    .where(function even(dot) { return dot.x % 2 === 0; })
+    .combineLatest(
+      dot
+        .where(function uneven(dot) { return dot.x % 2 !== 0; }),
+      function(prev, curr) {
+        return {value: curr.y, dots: {prev: prev, curr: curr}};
+      })
